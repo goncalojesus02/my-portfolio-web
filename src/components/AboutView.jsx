a28026032e17e9a0bc0104805f0f9497d21961d3
goncalojesus02 <gvffjesus@gmail.com>
2025-12-22 12:32:19 +0000

    update bd aboutview

--- src/components/AboutView.jsx
+++ src/components/AboutView.jsx
@@ -2,10 +2,10 @@ function AboutView({ project }) {
   return (
     <section className="pl-60 text-black">
         
-      <h1 className="text-4xl pb-4">{project.title}</h1>
-
-      <p className="py-6 whitespace-pre-line">{project.text1}</p>
-      <p className="py-6 whitespace-pre-line">{project.text2}</p>
+      <h1 className="text-4xl">{project.title}</h1>
+      <p className="py-2 whitespace-pre-line">{project.text1}</p>
+      <p className=" whitespace-pre-line">{project.linebreake}</p>
+      <p className="py-2 whitespace-pre-line">{project.text2}</p>
 
       <p className="mt-10 text-sm opacity-60">
         {project.year}
